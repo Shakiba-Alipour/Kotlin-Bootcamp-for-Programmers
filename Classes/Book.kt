07b9fe6bf7c4eeee3e18c4cfdf9518f37113a839
Shakiba-Alipour <alipour.sh79@gmail.com>
2022-12-01 17:08:38 +0330

    Create Book.kt

---
+++ Classes/Book.kt
@@ -0,0 +1,18 @@
+open class Book(){
+    var title : String
+    var author : String
+    override var currentPage : int = 0
+
+    open fun readPage(){
+        currentPage += 1
+    }
+}
+
+class eBook(){
+    var format : String = "text"
+    override var currentPage : int = 0
+
+    override fun readPage(){
+        currentPage += 250
+    }
+}
